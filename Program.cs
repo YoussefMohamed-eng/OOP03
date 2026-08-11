0783ff96ffd64133e969ecf9f1c0d5a391dc7bd0
YoussefMohamed-eng <ym6603158@gmail.com>
2026-08-11 11:06:02 +0300

    Question 01

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-          
+            #region Question 01
+            ///a)  What is the difference between Method Overloading and Method Overriding?
+            // overloading is when you have multiple methods in the same class with the same name but different parameters (different type or number of parameters).
+            // Overriding is when a derived class provides a specific implementation of a method that is already defined in its base class.
+            // Overriding requires inheritance and the use of the 'virtual' and 'override' keywords.
+            /// b)  What is the difference between Static Binding and Dynamic Binding?
+            // Static binding (also known as early binding) occurs at compile time, where the method to be invoked is determined based on the reference type.
+            // Dynamic binding (also known as late binding) occurs at runtime, where the method to be invoked is determined based on the actual object type.
+            #endregion
         }
     }
 }
